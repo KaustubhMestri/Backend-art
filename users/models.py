2d06e8d18e817c3a5432566cdb2adf00f5f1fe27
Kaustubh Mestri <130823605+KaustubhMestri@users.noreply.github.com>
2024-05-03 10:37:43 +0530

    Admin Login and Register

--- users/models.py
+++ users/models.py
@@ -4,4 +4,10 @@ class Users(models.Model):
     username = models.CharField(max_length=255)
     email = models.EmailField()
     phone = models.CharField(max_length=10)
-    password = models.CharField(max_length=255)
+    password = models.CharField(max_length=250)
+
+class Admin(models.Model):
+    username = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=10)
+    password = models.CharField(max_length=250)
--- users/views.py
+++ users/views.py
@@ -32,8 +32,49 @@ def login(request):
     if request.method=='POST':
         username = request.POST.get('username')
         password = request.POST.get('passw')
+        print(username,sha256(password.encode('utf-8')).hexdigest())
         try:
             user = Users.objects.filter(email=username,password=sha256(password.encode('utf-8')).hexdigest())
+            if len(user)==1:
+                return JsonResponse({'status':'200','message':'Login Successfully'})
+            elif len(user)>1:
+                return JsonResponse({'status':'403','message':'Bad request'})
+            else:
+                return JsonResponse({'status':'404','message':'User not found'})
+        except Exception as e:
+            print(e)
+            return JsonResponse({'status':'500','message':'Internal Server Error!'})
+        
+
+@csrf_exempt
+def registerAdmin(request):
+    if request.method=='POST':
+        nameAd =request.POST.get('nameAd')
+        emailAd = request.POST.get('emailAd')
+        mobileAd = request.POST.get('mobileAd')
+        passwordAd = request.POST.get('passwAd')
+        print(mobileAd)
+     
+        try:
+            user_obj = Users.objects.filter(email=emailAd)
+            if len(user_obj)==0:
+                users = Users(username=nameAd,email=emailAd,phone=str(mobileAd),password=sha256(passwordAd.encode('utf-8')).hexdigest())
+                users.save()
+                return JsonResponse({'status':'200','message':'Users account created successful!'})
+            else:
+                return JsonResponse({'status':'409','message':'Users already exists'})
+        except Exception as e:
+            print(e)
+            return JsonResponse({'status':'500','message':'Internal Server Error!'})
+        
+@csrf_exempt    
+def loginAdmin(request):
+    if request.method=='POST':
+        usernameAd = request.POST.get('username')
+        passwordAd = request.POST.get('passw')
+        print(usernameAd,sha256(passwordAd.encode('utf-8')).hexdigest())
+        try:
+            user = Users.objects.filter(email=usernameAd,password=sha256(passwordAd.encode('utf-8')).hexdigest())
             if len(user)==1:
                 return JsonResponse({'status':'200','message':'Login Successfully'})
             elif len(user)>1:
