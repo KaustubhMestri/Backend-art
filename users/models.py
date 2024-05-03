5ba92c3a668b38e68cd59c3967f6e10ca602a905
Kaustubh Mestri <130823605+KaustubhMestri@users.noreply.github.com>
2024-05-03 12:26:11 +0530

    ProductModel

--- users/models.py
+++ users/models.py
@@ -11,3 +11,16 @@ class Admin(models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length=10)
     password = models.CharField(max_length=250)
+
+class Product(models.Model):
+    uniqueid = models.IntegerField(max_length=10)
+    productname = models.CharField(max_length=255)
+    productdescription = models.CharField(max_length=255)
+    productprice = models.IntegerField(max_length=20)
+    deliverycharge = models.IntegerField(max_length=20)
+    dealerId = models.IntegerField(max_length=10)
+    dealerName = models.CharField(max_length=20)
+    productImg1 = models.ImageField()
+    productImg2 = models.ImageField()
+    productImg3 = models.ImageField()
+    productImg4 = models.ImageField()
